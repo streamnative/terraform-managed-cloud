cd6bb2f7473d9f14b6a4d9cc01984d04bc0cacd9
Yisheng Cai <yisheng.cai@streamnative.io>
2023-10-25 00:31:56 +0800

    Support GCP impersonation

--- modules/gcp/vendor-access/main.tf
+++ modules/gcp/vendor-access/main.tf
@@ -1,13 +1,4 @@
 locals {
-  streamnative_gsa = concat(var.streamnative_vendor_access_gsa, var.streamnative_support_access_gsa)
-  iam_bindings = flatten([
-    for role in var.roles : [
-      for gsa in local.streamnative_gsa : {
-        role : role,
-        member : format("serviceAccount:%s", gsa),
-      }
-    ]
-  ])
   google_services = concat([
     "autoscaling.googleapis.com",
     "cloudresourcemanager.googleapis.com",
@@ -31,23 +22,86 @@ resource "google_project_service" "gcp_apis" {
   service            = local.google_services[count.index]
 }
 
+locals {
+  is_impersonation_enabled = var.streamnative_org_id != ""
+  streamnative_gsa         = concat(var.streamnative_vendor_access_gsa, var.streamnative_support_access_gsa)
+}
+
+# Grant permissions directly to the StreamNative Cloud service account
+locals {
+  iam_bindings = local.is_impersonation_enabled ? [] : flatten([
+    for role in var.roles : [
+      for gsa in local.streamnative_gsa : {
+        role : role,
+        member : format("serviceAccount:%s", gsa),
+      }
+    ]
+  ])
+}
+
 resource "google_project_iam_member" "sn_access" {
-  for_each = {
-    for index, binding in local.iam_bindings :
-    index => binding
-  }
+  count = length(local.iam_bindings)
   project    = var.project
-  role       = each.value.role
-  member     = each.value.member
+  role       = local.iam_bindings[count.index].role
+  member     = local.iam_bindings[count.index].member
   depends_on = [google_project_service.gcp_apis]
 }
 
+# Grant permissions to the project service account that will be impersonated by StreamNative Cloud service account
+locals {
+  streamnative_bootstrap_gsa_name = format("snbootstrap-%s", var.streamnative_org_id)
+  streamnative_bootstrap_roles    = local.is_impersonation_enabled ? var.roles : []
+  impersonation_roles = [
+    "roles/iam.serviceAccountUser",
+    "roles/iam.serviceAccountAdmin",
+    "roles/iam.serviceAccountTokenCreator",
+  ]
+  impersonation_iam_bindings = local.is_impersonation_enabled ? flatten([
+    for role in local.impersonation_roles : [
+      for gsa in local.streamnative_gsa : {
+        role : role,
+        member : format("serviceAccount:%s", gsa),
+      }
+    ]
+  ]) : []
+
+}
+resource "google_service_account" "sn_bootstrap" {
+  count        = local.is_impersonation_enabled ? 1 : 0
+  account_id   = local.streamnative_bootstrap_gsa_name
+  project      = var.project
+  display_name = "StreamNative Bootstrap GSA that will be imporsonated by StreamNative Cloud Control Plane and Support Roles."
+  depends_on   = [google_project_service.gcp_apis]
+}
+
+resource "google_project_iam_member" "sn_bootstrap" {
+  count      = length(local.streamnative_bootstrap_roles)
+  project    = var.project
+  role       = local.streamnative_bootstrap_roles[count.index]
+  member     = format("serviceAccount:%s", google_service_account.sn_bootstrap[0].email)
+  depends_on = [google_service_account.sn_bootstrap]
+}
+
+resource "google_service_account_iam_member" "sn_bootstrap_impersonation" {
+  count              = length(local.impersonation_iam_bindings)
+  service_account_id = google_service_account.sn_bootstrap[0].id
+  role               = local.impersonation_iam_bindings[count.index].role
+  member             = local.impersonation_iam_bindings[count.index].member
+  depends_on         = [google_service_account.sn_bootstrap]
+}
+
+
 output "google_services" {
-  value = local.google_services
+  value       = local.google_services
   description = "Enabled google services."
 }
 
 output "iam_bindings" {
-  value = local.iam_bindings
+  value       = local.iam_bindings
   description = "Configured iam policies."
+}
+
+output "impersonation_iam_bindings" {
+  value       = local.impersonation_iam_bindings
+  description = "Configured iam policies for impersonation."
 }
